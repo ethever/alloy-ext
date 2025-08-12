85a89a082a0216a5dd6980a99b85ca6b404fd12c
ethever <25705974+ethever@users.noreply.github.com>
2025-08-12 14:21:58 +0800

    fallback layer

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub use check_revert::ReceiptExt;
+pub use fallback::FallbackLayer;
 pub use read_write_provider::ReadWriteProvider;
 pub use transports::MultiTransport;
 
 mod check_revert;
+mod fallback;
 mod read_write_provider;
 mod transports;
